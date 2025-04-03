281f08cef102488db934c95feaff91a74f50cb87
Mark Malstrom <mark@malstrom.me>
2025-04-02 23:06:27 -0500

    Add flat routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,3 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { flatRoutes } from "@react-router/fs-routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default flatRoutes();
