51d682762e64ea8eed9881b687df276336acfcf5
Brad Garropy <bradgarropy@gmail.com>
2025-04-17 11:40:23 -0500

    Cloudflare vite plugin (#21)

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
-import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { getLoadContext } from "./load-context";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
-export default defineConfig(({ isSsrBuild }) => ({
-    build: {
-        rollupOptions: isSsrBuild
-            ? {
-                  input: "./workers/app.ts",
-              }
-            : undefined,
-    },
-    ssr: {
-        target: "webworker",
-        noExternal: true,
-        resolve: {
-            conditions: ["workerd", "browser"],
-        },
-        optimizeDeps: {
-            include: [
-                "react",
-                "react/jsx-runtime",
-                "react/jsx-dev-runtime",
-                "react-dom",
-                "react-dom/server",
-                "react-router",
-            ],
-        },
-    },
+export default defineConfig({
     plugins: [
-        cloudflareDevProxy({
-            getLoadContext,
-        }),
+        cloudflare({ viteEnvironment: { name: "ssr" } }),
         tailwindcss(),
         reactRouter(),
         tsconfigPaths(),
     ],
-}));
+});
