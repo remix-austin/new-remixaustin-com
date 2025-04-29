86552154830e3f1226887dbfdcc6084b4070b71a
Brad Garropy <bradgarropy@gmail.com>
2025-04-29 07:05:15 -0500

    add form parser helper.

---
+++ app/utils/forms.ts
@@ -0,0 +1,14 @@
+import type {z, ZodTypeAny} from "zod"
+
+const parseFormData = async <T extends ZodTypeAny>(
+    request: Request,
+    schema: T,
+) => {
+    const formData = await request.formData()
+    const data = Object.fromEntries(formData.entries())
+
+    const parsedFormData = schema.parse(data) as z.infer<T>
+    return parsedFormData
+}
+
+export {parseFormData}
