ea4f7b18de32862dc6eea87fe3fe841bf31e84a0
Brad Garropy <bradgarropy@gmail.com>
2025-04-30 13:10:51 -0500

    Submissions (#34)

---
+++ drizzle/0001_abandoned_secret_warriors.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `talks` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	`phone` text NOT NULL,
+	`title` text NOT NULL,
+	`description` text NOT NULL,
+	`status` text DEFAULT 'submitted',
+	`date` text,
+	`link` text,
+	`updatedAt` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`createdAt` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL
+);
