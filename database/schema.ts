ddebf44414c0fa1d8e99ecff3a44b0490c8b434b
Brad Garropy <bradgarropy@gmail.com>
2025-04-30 08:45:44 -0500

    define talk schema.

--- database/schema.ts
+++ database/schema.ts
@@ -1,7 +1,28 @@
+import { sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const guestBook = sqliteTable("guestBook", {
+const timestamps = {
+    updatedAt: integer(),
+    createdAt: integer().default(sql`(CURRENT_TIMESTAMP)`).notNull(),
+}
+
+const guestBook = sqliteTable("guestBook", {
     id: integer().primaryKey({ autoIncrement: true }),
     name: text().notNull(),
     email: text().notNull().unique(),
 });
+
+const talk = sqliteTable("talk", {
+    id: integer().primaryKey({ autoIncrement: true }).notNull(),
+    name: text().notNull(),
+    email: text().notNull(),
+    phone: text().notNull(),
+    title: text().notNull(),
+    description: text().notNull(),
+    status: text({enum: ["submitted", "rejected", "scheduled", "completed"]}).notNull(),
+    date: text().default(sql`(CURRENT_DATE)`),
+    link: text(),
+    ...timestamps,
+})
+
+export {guestBook, talk}
