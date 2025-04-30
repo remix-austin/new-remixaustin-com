91b156222cd7e45f35379639a2e4728d3163211a
Brad Garropy <bradgarropy@gmail.com>
2025-04-30 09:10:50 -0500

    create migration.

--- drizzle/0001_mature_chameleon.sql
+++ drizzle/0001_mature_chameleon.sql
@@ -1,12 +1,12 @@
-CREATE TABLE `talk` (
+CREATE TABLE `talks` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`name` text NOT NULL,
 	`email` text NOT NULL,
 	`phone` text NOT NULL,
 	`title` text NOT NULL,
 	`description` text NOT NULL,
-	`status` text NOT NULL,
-	`date` text DEFAULT (CURRENT_DATE),
+	`status` text DEFAULT 'submitted',
+	`date` text,
 	`link` text,
 	`updatedAt` integer,
 	`createdAt` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL
