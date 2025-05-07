d60bd648aae40db00f2130482d0d25e544d906af
Brad Garropy <bradgarropy@gmail.com>
2025-05-07 12:50:38 -0500

    switch to layout route.

--- app/routes/_page.talks._index/route.tsx
+++ app/routes/_page.talks._index/route.tsx
@@ -1,8 +1,8 @@
 export default function Talks() {
     return (
-        <div className="p-20">
+        <>
             <title>Remix Austin | Talks</title>
             <h1>Talks</h1>
-        </div>
+        </>
     );
 }
