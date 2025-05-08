6a16964449e82963d64e0312091428fb686773f8
Brad Garropy <bradgarropy@gmail.com>
2025-05-08 11:35:09 -0500

    created meetup graphql client. defined get events method.

---
+++ app/utils/meetup.ts
@@ -0,0 +1,43 @@
+import { Client, gql, cacheExchange, fetchExchange } from "@urql/core";
+
+const client = new Client({
+    url: "https://api.meetup.com/gql-ext",
+    exchanges: [cacheExchange, fetchExchange],
+});
+
+const MeetupQuery = gql`
+    query ($urlname: String!) {
+        groupByUrlname(urlname: $urlname) {
+            id
+            name
+            currentEvent:events(status:ACTIVE, sort:DESC, first:1) {
+                totalCount
+                edges {
+                    node {
+                        dateTime
+                        description
+                    }
+                }
+            }
+            pastEvents:events(status:PAST, sort:DESC, first:5) {
+                totalCount
+                edges {
+                    node {
+                        title
+                        dateTime
+                        rsvps {
+                            yesCount
+                        }
+                    }
+                }
+            }
+        }
+    }`;
+
+const getEvents = async () => {
+    const response = await client.query(MeetupQuery, { urlname: "remix-austin" }).toPromise();
+    const events = response.data.groupByUrlname;
+    return events;
+};
+
+export { getEvents };
