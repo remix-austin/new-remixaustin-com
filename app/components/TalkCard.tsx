50d09ccce6d98a15a1f39c123ce30e4884b4b99d
Brad Garropy <bradgarropy@gmail.com>
2025-05-12 07:43:00 -0500

    add placeholder image to the talk card.

--- app/components/TalkCard.tsx
+++ app/components/TalkCard.tsx
@@ -10,10 +10,18 @@ type TalkCardProps = {
 const TalkCard: FC<TalkCardProps> = ({ talk }) => {
     return (
         <Link target="_blank" to={talk.eventUrl}>
-            <article className="rounded-lg border-2 p-8">
-                <p>{formatDate(talk.dateTime)}</p>
-                <h4 className="mb-6 font-bold text-3xl">{talk.title}</h4>
-                <p>{`${talk.rsvps.yesCount} attendees`}</p>
+            <article className="grid grid-cols-[auto_1fr] gap-x-8 rounded-lg border-2 p-8">
+                <img
+                    alt={talk.title}
+                    className="h-64 w-full border-2 object-cover"
+                    src="https://i.ytimg.com/vi/kMa4zmvlHFI/maxresdefault.jpg"
+                />
+
+                <div>
+                    <p>{formatDate(talk.dateTime)}</p>
+                    <h4 className="mb-6 font-bold text-3xl">{talk.title}</h4>
+                    <p>{`${talk.rsvps.yesCount} attendees`}</p>
+                </div>
             </article>
         </Link>
     );
